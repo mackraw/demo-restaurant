c837eda867c75650ae0e2e1fe83809259fa47315
mackraw <maciekrawczyk@gmail.com>
2021-08-19 23:47:27 -0500

    added shaddow to burger button

--- common.js
+++ common.js
@@ -3,11 +3,11 @@
 // make nav smaller when scrolling down
 const nav = document.getElementById('nav');
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 100) {
+  if (window.scrollY > 100 && window.screen.width > 600) {
     nav.style.backgroundColor = `hsla(0, 0%, 25%, 0.95)`;
     nav.style.height = `5rem`;
   }
-  if (window.scrollY < 100) {
+  if (window.scrollY < 100 && window.screen.width > 600) {
     nav.style.backgroundColor = `hsla(0, 0%, 25%, 0)`;
     nav.style.height = `7rem`;
   }
